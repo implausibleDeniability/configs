9a040e5feeed0e63637f5d1c3032dfacb1ccdbf0
Maxim Faleev <m.faleev@innopolis.university>
2022-02-28 20:10:04 +0300

    fixed pyright server installation for neovim

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,11 +6,12 @@ apt-get update
 apt-get install -y neovim
 
 # install pyright
-apt-get install -y npm
-npm install pyright
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
+npm install -g pyright
 
 # install vim plugin manager
-apt-get install curl
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
