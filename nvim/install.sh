5ab30ec0e341cc9b795bd199307ba7ff61a17f02
Maxim Faleev <m.faleev@innopolis.university>
2022-02-28 19:34:06 +0300

    + nvim dependencies installation script

---
+++ nvim/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install neovim
+apt install software-properties-common
+add-apt-repository ppa:neovim-ppa/unstable
+apt-get update
+apt-get install -y neovim
+
+# install pyright
+apt-get install -y npm
+npm install pyright
+
+# install vim plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# symbolic link
+ln -s ~/.configs/nvim/init.vim ~/configs/nvim/init.vim
+
+# further instructions
+echo "In ~/.configs/nvim/init.vim run ':so %', then ':PlugInstall', then ':so %' again"
+
